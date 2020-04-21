1ba6a645849092b25261c4c94460fa2b68836b50
Pdugovich <pdugovich@Gmail.com>
2020-04-21 11:43:06 -0400

    completed day 1 assignment

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,19 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # Looking further down the file, I can see what they expect to be here
+        # We need a register which they expect to be self.reg, which holds 8
+        self.reg = [0] * 8
+        # We need memory, which looks like they expect to be self.ram
+        self.ram = [0] * 256
+        # Probably a program counter also
+        self.pc = 0
+    
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, to_write, address):
+        self.ram[address] = to_write
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +74,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.pc = 0
+        running = True
+        while running == True:
+            # Read the memory stored at current pc
+            # Store that value in Instruction Register(IR)
+            ir = self.ram_read(self.pc)
+
+            # Using ram_read(), store bytes pc+1 and + 2
+            # into variables operand_a and b in case
+            # the instructions call for them
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            # Then, depending(if) on the opcode, perform
+            # the action 
+            if ir == 0b00000001:
+                running = False
+            elif ir == 0b10000010:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == 0b01000111:
+                print(self.reg[operand_a])
+                self.pc +=2
+
+            # Then, increment PC appropriately.
+            # The number of bytes an instruction uses
+            # can be determined from bits 6 and 7 
+            # in instruction opcode. See ls8 specs
